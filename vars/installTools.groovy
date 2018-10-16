6de447df73382343d8420140e0aec391cd9eb9d3
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-16 15:19:26 -0400

    pipenv skip-lock
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/installTools.groovy
+++ vars/installTools.groovy
@@ -6,5 +6,5 @@ def call() {
     sh "sudo -H snap install kubectl --classic || true"
     sh "sudo -H snap install bundletester --edge --classic || true"
     sh "sudo -H pip3 install pipenv"
-    sh "cd jobs && /usr/local/bin/pipenv install"
+    sh "cd jobs && /usr/local/bin/pipenv install --skip-lock"
 }
