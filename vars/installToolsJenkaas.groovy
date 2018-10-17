9b8c5fc3ff0b5c6907ea8c4f599a119a5fef8ee2
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-17 17:08:16 -0400

    Add jenkaas install tools
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

---
+++ vars/installToolsJenkaas.groovy
@@ -0,0 +1,15 @@
+/* Installs required tools for testing
+ *
+ */
+def call() {
+    sh "sudo -H -E python3 -m pip install -U pipenv"
+    sh "cd jobs && /usr/local/bin/pipenv install --skip-lock"
+
+    // Charmstore auth
+    withCredentials([file(credentialsId: 'charm_creds', variable: 'CHARMCREDS'),
+                     file(credentialsId: 'juju_creds', variable: 'JUJUCREDS'),
+                     file(credentialsId: 'snapcraft_creds', variable: 'SNAPCRAFTCREDS')]) {
+
+        sh "cd jobs/infra && /usr/local/bin/pipenv run ansible-playbook playbook-jenkins.yml --extra-vars 'charm_creds=${CHARMCREDS} juju_creds=${JUJUCREDS} snapcraft_creds=${SNAPCRAFTCREDS}'"
+    }
+}
