949fc818ef769bf98a5778f87d109a3c8756f1f3
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-19 10:25:38 -0400

    update collection script
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/collectDebug.groovy
+++ vars/collectDebug.groovy
@@ -5,17 +5,7 @@
  */
 def call(String controller,
          String model) {
-    dir("debug-out") {
-        sh "juju status -m ${controller}:${model} --format yaml | tee juju-status.yaml"
-        sh "juju debug-log -m ${controller}:${model} --replay | tee debug-log.txt"
-        sh "juju storage -m ${controller}:${model} --format yaml | tee storage.yaml"
-        sh "juju storage-pools -m ${controller}:${model} --format yaml | tee storage-pools.yaml"
-        sh "juju config -m ${controller}:${model} kubernetes-master --format yaml | tee kubernetes-master.yaml"
-        sh "juju config -m ${controller}:${model} kubernetes-worker --format yaml | tee kubernetes-worker.yaml"
-        sh "juju config -m ${controller}:${model} kubernetes-load-balancer --format yaml | tee kubernetes-load-balancer.yaml"
-        sh "juju config -m ${controller}:${model} etcd --format yaml | tee etcd.yaml"
-        sh "juju config -m ${controller}:${model} easyrsa --format yaml | tee easyrsa.yaml"
-        sh "juju config -m ${controller}:${model} flannel --format yaml | tee flannel.yaml"
-    }
-    archiveArtifacts artifacts: 'debug-out/*', fingerprint: true
+    sh "wget https://raw.githubusercontent.com/juju-solutions/cdk-field-agent/master/collect.py"
+    sh "pipenv run python3 collect.py -m ${controller}:${model}"
+    archiveArtifacts artifacts: 'results**', fingerprint: true
 }
