bbccfbace2251c89d866f8592a0fbb5e97536251
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-21 14:01:14 -0400

    use ppa ansible
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/installToolsJenkaas.groovy
+++ vars/installToolsJenkaas.groovy
@@ -4,18 +4,17 @@
  */
 def call() {
     sh "sudo add-apt-repository -y ppa:deadsnakes/ppa"
+    sh "sudo add-apt-repository -y ppa:ansible/ansible"
     sh "sudo apt update"
-    sh "sudo apt install -qyf libffi-dev python3.6 python3.6-dev python3-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev"
-    sh "sudo -H -E python3.5 -m pip install -U pipenv"
-    sh "sudo -H -E python3.6 -m pip install -U pipenv"
-    sh "cd jobs && python3.5 -m pipenv --python 3.5 install --skip-lock"
-    sh "cd jobs && python3.6 -m pipenv --python 3.6 install --skip-lock"
+    sh "sudo apt install -qyf ansible"
+    sh "sudo -H -E python3.5 -m pip uninstall pipenv"
+    sh "sudo -H -E python3.6 -m pip uninstall pipenv"
 
     // Charmstore auth
     withCredentials([file(credentialsId: 'charm_creds', variable: 'CHARMCREDS'),
                      file(credentialsId: 'juju_creds', variable: 'JUJUCREDS'),
                      file(credentialsId: 'snapcraft_creds', variable: 'SNAPCRAFTCREDS')]) {
 
-        sh "cd jobs/infra && pipenv --python 3.5 run ansible-playbook playbook-jenkins.yml --extra-vars 'charm_creds=${CHARMCREDS} juju_creds=${JUJUCREDS} snapcraft_creds=${SNAPCRAFTCREDS}'"
+        sh "cd jobs/infra && ansible-playbook playbook-jenkins.yml --extra-vars 'charm_creds=${CHARMCREDS} juju_creds=${JUJUCREDS} snapcraft_creds=${SNAPCRAFTCREDS}'"
     }
 }
