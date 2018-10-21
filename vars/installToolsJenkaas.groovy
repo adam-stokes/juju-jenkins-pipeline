736511e18ae79ab27fbdaedf73dff17b8e55b268
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-21 13:34:04 -0400

    fix install
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/installToolsJenkaas.groovy
+++ vars/installToolsJenkaas.groovy
@@ -8,8 +8,8 @@ def call() {
     sh "sudo apt install -qyf libffi-dev python3.6 python3.6-dev python3-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev"
     sh "sudo -H -E python3.5 -m pip install -U pipenv"
     sh "sudo -H -E python3.6 -m pip install -U pipenv"
-    // sh "cd jobs && python3.5 -m pipenv --python 3.5 install --skip-lock"
-    // sh "cd jobs && python3.6 -m pipenv --python 3.6 install --skip-lock"
+    sh "cd jobs && python3.5 -m pipenv --python 3.5 install --skip-lock"
+    sh "cd jobs && python3.6 -m pipenv --python 3.6 install --skip-lock"
 
     // Charmstore auth
     withCredentials([file(credentialsId: 'charm_creds', variable: 'CHARMCREDS'),
