4d18eb0f0392fdf8a5cfd47b87932e9606249445
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-21 12:49:45 -0400

    pipenv
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/installToolsJenkaas.groovy
+++ vars/installToolsJenkaas.groovy
@@ -7,13 +7,13 @@ def call() {
     // sh "sudo apt install -qyf libffi-dev python3.6 python3.6-dev python3-dev libffi-dev libssl-dev libxml2-dev libxslt1-dev libjpeg8-dev zlib1g-dev"
     sh "sudo -H -E python3.6 -m pip install -U pipenv"
     sh "which pipenv"
-    sh "cd jobs && pipenv --python 3.6 install --skip-lock"
+    sh "cd jobs && python3.6 -m pipenv --python 3.6 install --skip-lock"
 
     // Charmstore auth
     withCredentials([file(credentialsId: 'charm_creds', variable: 'CHARMCREDS'),
                      file(credentialsId: 'juju_creds', variable: 'JUJUCREDS'),
                      file(credentialsId: 'snapcraft_creds', variable: 'SNAPCRAFTCREDS')]) {
 
-        sh "cd jobs/infra && pipenv run ansible-playbook playbook-jenkins.yml --extra-vars 'charm_creds=${CHARMCREDS} juju_creds=${JUJUCREDS} snapcraft_creds=${SNAPCRAFTCREDS}'"
+        sh "cd jobs/infra && python3.6 -m pipenv run ansible-playbook playbook-jenkins.yml --extra-vars 'charm_creds=${CHARMCREDS} juju_creds=${JUJUCREDS} snapcraft_creds=${SNAPCRAFTCREDS}'"
     }
 }
