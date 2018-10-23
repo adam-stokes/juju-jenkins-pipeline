7199532a9fbb484246ba6587d06554a352c3dcb0
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-23 10:33:20 -0400

    add jaas creds
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/installToolsJenkaas.groovy
+++ vars/installToolsJenkaas.groovy
@@ -11,10 +11,12 @@ def call() {
     // Charmstore auth
     withCredentials([file(credentialsId: 'charm_creds', variable: 'CHARMCREDS'),
                      file(credentialsId: 'juju_creds', variable: 'JUJUCREDS'),
+                     file(credentialsId: 'jaas_login', variable: 'JAASCREDS'),
                      file(credentialsId: 'snapcraft_creds', variable: 'SNAPCRAFTCREDS')]) {
 
         sh "export CHARMCREDS=${CHARMCREDS}"
         sh "export JUJUCREDS=${JUJUCREDS}"
+        sh "export JAASCREDS=${JAASCREDS}"
         sh "export SNAPCRAFTCREDS=${SNAPCRAFTCREDS}"
         sh "cd jobs && tox -e py35 -- ansible-playbook infra/playbook-jenkins.yml -e 'ansible_python_interpreter=/usr/bin/python3.5'"
     }
