28626b267d44918631a0f537dac077751bcf99c7
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-23 16:49:46 -0400

    do a map
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/deployCDK.groovy
+++ vars/deployCDK.groovy
@@ -5,20 +5,23 @@
  * @param version_overlay path to bundle overlay for defining k8s versions
  * @param bundle_channel juju charmstore channel to pull bundle from
  */
-def call(String controller,
-         String model,
-         String bundle,
-         String version_overlay,
-         String cloud = 'aws',
-         String bundle_channel = 'edge',
-         Boolean allow_privileged = false) {
-    sh "juju add-model -c ${controller} ${model} ${cloud}"
-    sh "juju deploy -m ${controller}:${model} ${bundle} --overlay ${version_overlay} --channel ${bundle_channel}"
-    if (allow_privileged) {
-        sh "juju config -m ${controller}:${model} kubernetes-master allow-privileged=true"
-        sh "juju config -m ${controller}:${model} kubernetes-worker allow-privileged=true"
+def call(Map conf) {
+    if (!conf.cloud) {
+        conf.cloud = 'aws'
+    }
+    if (!conf.bundle_channel) {
+        conf.bundle_channel = 'edge'
+    }
+    if (!conf.allow_privileged) {
+        conf.allow_privileged = false
+    }
+    sh "juju add-model -c ${conf.controller} ${conf.model} ${conf.cloud}"
+    sh "juju deploy -m ${conf.controller}:${conf.model} ${conf.bundle} --overlay ${conf.version_overlay} --channel ${conf.bundle_channel}"
+    if (conf.allow_privileged) {
+        sh "juju config -m ${conf.controller}:${conf.model} kubernetes-master allow-privileged=true"
+        sh "juju config -m ${conf.controller}:${conf.model} kubernetes-worker allow-privileged=true"
     }
     retry(5){
-        sh "juju-wait -e ${controller}:${model} -w"
+        sh "juju-wait -e ${conf.controller}:${conf.model} -w"
     }
 }
