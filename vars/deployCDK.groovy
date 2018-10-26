6fbb197be7b053b2e2b0c425152ab17ed4edcc38
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-26 10:09:20 -0400

    disable add model
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/deployCDK.groovy
+++ vars/deployCDK.groovy
@@ -1,9 +1,14 @@
 /* Deploy CDK
  *
- * @param controller juju controller to use
- * @param model juju model to use
- * @param version_overlay path to bundle overlay for defining k8s versions
- * @param bundle_channel juju charmstore channel to pull bundle from
+ * @param conf Map
+ *    conf.controller juju controller to use
+ *    conf.model juju model to use
+ *    conf.cloud juju cloud in a cloud/region format
+ *    conf.version_overlay path to bundle overlay for defining k8s versions
+ *    conf.bundle_channel juju charmstore channel to pull bundle from
+ *    conf.allow_privileged enable allow privileged in k8s master/worker
+ *    conf.disable_add_model do not run add-model, this is due to factors such
+ *                           as localhost that need lxd profiles update prior to deploy
  */
 def call(Map conf) {
     if (!conf.cloud) {
@@ -15,10 +20,12 @@ def call(Map conf) {
     if (!conf.allow_privileged) {
         conf.allow_privileged = false
     }
-    if (conf.cloud) {
-        sh "juju add-model -c ${conf.controller} ${conf.model} ${conf.cloud}"
-    } else {
-        sh "juju add-model -c ${conf.controller} ${conf.model}"
+    if (!conf.disable_add_model) {
+        if (conf.cloud) {
+            sh "juju add-model -c ${conf.controller} ${conf.model} ${conf.cloud}"
+        } else {
+            sh "juju add-model -c ${conf.controller} ${conf.model}"
+        }
     }
     sh "juju deploy -m ${conf.controller}:${conf.model} ${conf.bundle} --overlay ${conf.version_overlay} --channel ${conf.bundle_channel}"
     if (conf.allow_privileged) {
