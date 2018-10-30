45d852789f8a7c3e34d2a4bf7af7e3cb3bd7d72d
Adam Stokes <battlemidget@users.noreply.github.com>
2018-10-30 14:48:41 -0400

    add awscreds env
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/installToolsJenkaas.groovy
+++ vars/installToolsJenkaas.groovy
@@ -12,12 +12,14 @@ def call() {
     withCredentials([file(credentialsId: 'charm_creds', variable: 'CHARMCREDS'),
                      file(credentialsId: 'juju_creds', variable: 'JUJUCREDS'),
                      file(credentialsId: 'jaas_login', variable: 'JAASCREDS'),
+                     file(credentialsId: 'aws_creds', variable: 'AWSCREDS'),
                      file(credentialsId: 'snapcraft_creds', variable: 'SNAPCRAFTCREDS')]) {
 
         sh "export CHARMCREDS=${CHARMCREDS}"
         sh "export JUJUCREDS=${JUJUCREDS}"
         sh "export JAASCREDS=${JAASCREDS}"
         sh "export SNAPCRAFTCREDS=${SNAPCRAFTCREDS}"
+        sh "export AWSCREDS=${AWSCREDS}"
         sh "cd jobs && tox -e py35 -- ansible-playbook infra/playbook-jenkins.yml -e 'ansible_python_interpreter=/usr/bin/python3.5'"
     }
 }
