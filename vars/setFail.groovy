d9036d6dfa42110251c6f00a56c68006e41b1999
Adam Stokes <battlemidget@users.noreply.github.com>
2019-02-28 20:53:44 +0000

    save meta
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/setFail.groovy
+++ vars/setFail.groovy
@@ -4,4 +4,5 @@
 def call() {
     def collect_debug_sh = "tox -e py36 -- python3 infra/collect-debug.py"
     sh "cd jobs && ${collect_debug_sh} test-result --fail"
+    sh "cd jobs && ${collect_debug_sh} save-meta"
 }
