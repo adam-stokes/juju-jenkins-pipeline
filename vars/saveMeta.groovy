3e7f6d8ce246b60fa412736667394470c07d7c24
Adam Stokes <battlemidget@users.noreply.github.com>
2019-03-01 11:02:06 +0000

    Update savemeta
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/saveMeta.groovy
+++ vars/saveMeta.groovy
@@ -1,7 +1,7 @@
-/* Sets a failure status
+/* Saves metadata
  *
  */
 def call() {
     def collect_debug_sh = "tox -e py36 -- python3 infra/collect-debug.py"
-    sh "cd jobs && ${collect_debug_sh} test-result --fail"
+    sh "cd jobs && ${collect_debug_sh} save-meta"
 }
