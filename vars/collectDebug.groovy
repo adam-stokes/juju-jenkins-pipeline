8f4d84d92b13a31eb90f006b358681bbd38d7966
Adam Stokes <battlemidget@users.noreply.github.com>
2019-03-25 14:26:41 -0400

    fix workdir again
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/collectDebug.groovy
+++ vars/collectDebug.groovy
@@ -5,9 +5,9 @@
  */
 def call(String controller,
          String model) {
-    def collect_debug_sh = "tox -e py36 --workdir ${env.WORKSPACE} -- python3 infra/collect-debug.py"
+    def collect_debug_sh = "tox -e py36 -- python3 infra/collect-debug.py"
     sh "cd jobs && wget https://raw.githubusercontent.com/juju-solutions/cdk-field-agent/master/collect.py"
-    sh "cd jobs && tox -e py36 --workdir ${env.WORKSPACE} -- python3 collect.py -m ${controller}:${model}"
+    sh "cd jobs && tox -e py36 -- python3 collect.py -m ${controller}:${model}"
     sh "cd jobs && ${collect_debug_sh} push --key-id 'field-agent' results*.tar.gz"
 
     archiveArtifacts artifacts: 'jobs/results**', fingerprint: true
