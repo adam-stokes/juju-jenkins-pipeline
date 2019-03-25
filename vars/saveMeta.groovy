a35debaababaaa53698ff68ca988310d02431574
Adam Stokes <battlemidget@users.noreply.github.com>
2019-03-25 14:21:14 -0400

    fix workdir
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/saveMeta.groovy
+++ vars/saveMeta.groovy
@@ -2,7 +2,7 @@
  *
  */
 def call() {
-    def collect_debug_sh = "tox -e py36 -- python3 infra/collect-debug.py"
+    def collect_debug_sh = "tox -e py36 --workdir ${env.WORKSPACE} -- python3 infra/collect-debug.py"
     sh "cd jobs && ${collect_debug_sh} set-meta"
     sh "cd jobs && ${collect_debug_sh} save-meta"
     sh "cd jobs && ${collect_debug_sh} push meta.yaml"
--- vars/setEndTime.groovy
+++ vars/setEndTime.groovy
@@ -2,6 +2,6 @@
  *
  */
 def call() {
-    def collect_debug_sh = "tox -e py36 -- python3 infra/collect-debug.py"
+    def collect_debug_sh = "tox -e py36 --workdir ${env.WORKSPACE} -- python3 infra/collect-debug.py"
     sh "cd jobs && ${collect_debug_sh} endtime"
 }
