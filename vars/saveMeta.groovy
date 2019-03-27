aad9a1c20491a4df235123efdc157e81bbfafca1
Adam Stokes <battlemidget@users.noreply.github.com>
2019-03-27 11:59:54 -0400

    try db
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/saveMeta.groovy
+++ vars/saveMeta.groovy
@@ -5,6 +5,6 @@ def call() {
     def collect_debug_sh = "tox -e py36 -- python3 infra/collect-debug.py"
     sh "cd jobs && ${collect_debug_sh} set-meta"
     sh "cd jobs && ${collect_debug_sh} save-meta"
-    sh "cd jobs && ${collect_debug_sh} push meta.yaml"
-    sh "cd jobs && ${collect_debug_sh} push stats.db"
+    // sh "cd jobs && ${collect_debug_sh} push meta.yaml"
+    // sh "cd jobs && ${collect_debug_sh} push stats.db"
 }
