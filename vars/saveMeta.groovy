fa2cea9b9a9b9533cdec68a484f6afc6ad595b63
Adam Stokes <battlemidget@users.noreply.github.com>
2019-04-01 15:50:02 -0400

    more path updates
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/saveMeta.groovy
+++ vars/saveMeta.groovy
@@ -2,8 +2,7 @@
  *
  */
 def call() {
-    def py3 = "/var/lib/jenkins/venvs/ci/bin/python3"
-    def collect_debug_sh = "${py3} infra/collect-debug.py"
+    def collect_debug_sh = "${utils.cipy} infra/collect-debug.py"
     sh "cd jobs && ${collect_debug_sh} set-meta"
     sh "cd jobs && ${collect_debug_sh} save-meta"
 }
--- vars/setPass.groovy
+++ vars/setPass.groovy
@@ -2,6 +2,6 @@
  *
  */
 def call() {
-    def collect_debug_sh = "/var/lib/jenkins/venvs/ci/bin/python3 infra/collect-debug.py"
+    def collect_debug_sh = "${utils.cipy} infra/collect-debug.py"
     sh "cd jobs && ${collect_debug_sh} test-result --no-fail"
 }
