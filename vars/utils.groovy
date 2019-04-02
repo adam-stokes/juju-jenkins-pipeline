179854d5f8b4a29d6f620c07c31755bf8890c101
Adam Stokes <battlemidget@users.noreply.github.com>
2019-04-02 14:16:13 -0400

    add cipaths
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -3,3 +3,4 @@ import groovy.transform.Field
 @Field cibin = '/var/lib/jenkins/venvs/ci/bin'
 @Field cipy = "${cibin}/python3.6"
 @Field pytest = "${cibin}/pytest -v -s"
+@Field cipaths = "${cibin}:/snap/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin"
