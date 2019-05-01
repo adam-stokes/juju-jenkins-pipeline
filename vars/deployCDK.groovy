4f8fc04018928bcbe75621cea60aa1beb7daca2d
Adam Stokes <battlemidget@users.noreply.github.com>
2019-05-01 10:27:25 -0400

    reduce verbose on juju-wait
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/deployCDK.groovy
+++ vars/deployCDK.groovy
@@ -1,3 +1,4 @@
+
 /* Deploy CDK
  *
  * @param conf Map
@@ -36,26 +37,26 @@ def call(Map conf) {
     }
     if (!conf.disable_add_model) {
         if (conf.cloud) {
-            sh "juju add-model -c ${conf.controller} ${conf.model} ${conf.cloud} ${utils.debug_out}"
+            sh "juju add-model -c ${conf.controller} ${conf.model} ${conf.cloud}"
         } else {
-            sh "juju add-model -c ${conf.controller} ${conf.model} ${utils.debug_out}"
+            sh "juju add-model -c ${conf.controller} ${conf.model}"
         }
     }
     // There is no way to tell Juju to use a specific channel of a bundle but
     // to leave the contents unmodified, so we have to pull a local copy of the
     // channel that we want.
     if (conf.custom_bundle) {
-        sh "juju deploy -m ${conf.controller}:${conf.model} ${conf.custom_bundle} ${utils.debug_out}"
+        sh "juju deploy -m ${conf.controller}:${conf.model} ${conf.custom_bundle}"
     } else {
-        sh "charm pull ${conf.bundle} --channel ${conf.bundle_channel} ./bundle-to-test ${utils.debug_out}"
-        sh "juju deploy -m ${conf.controller}:${conf.model} ./bundle-to-test/bundle.yaml ${conf.version_overlay} ${conf.charms_channel} ${utils.debug_out}"
+        sh "charm pull ${conf.bundle} --channel ${conf.bundle_channel} ./bundle-to-test"
+        sh "juju deploy -m ${conf.controller}:${conf.model} ./bundle-to-test/bundle.yaml ${conf.version_overlay} ${conf.charms_channel}"
     }
     if (conf.allow_privileged) {
-        sh "juju config -m ${conf.controller}:${conf.model} kubernetes-master allow-privileged=true ${utils.debug_out}"
-        sh "juju config -m ${conf.controller}:${conf.model} kubernetes-worker allow-privileged=true ${utils.debug_out}"
+        sh "juju config -m ${conf.controller}:${conf.model} kubernetes-master allow-privileged=true"
+        sh "juju config -m ${conf.controller}:${conf.model} kubernetes-worker allow-privileged=true"
     }
 
     if (!conf.disable_wait) {
-        sh "juju-wait -e ${conf.controller}:${conf.model} -wv -r3 -t14400 ${utils.debug_out}"
+        sh "juju-wait -e ${conf.controller}:${conf.model} -w -r3 -t14400"
     }
 }
