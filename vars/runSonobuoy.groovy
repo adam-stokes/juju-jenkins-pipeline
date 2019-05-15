68d0ca7acdd02359f805b673830d1703395ea58a
Adam Stokes <battlemidget@users.noreply.github.com>
2019-05-15 12:45:15 -0400

    append --wait to sonobuoy
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/runSonobuoy.groovy
+++ vars/runSonobuoy.groovy
@@ -5,8 +5,9 @@
  */
 def call(String controller,
          String model) {
-        sh "mkdir -p $HOME/.kube"
-        sh "juju scp -m ${controller}:${model} kubernetes-master/0:config $HOME/.kube/"
-        sh "export RBAC_ENABLED=\$(kubectl api-versions | grep \"rbac.authorization.k8s.io/v1beta1\" -c)"
-        sh "/var/lib/jenkins/go/bin/sonobuoy run"
+    sh "mkdir -p $HOME/.kube"
+    sh "juju scp -m ${controller}:${model} kubernetes-master/0:config $HOME/.kube/"
+    sh "export RBAC_ENABLED=\$(kubectl api-versions | grep \"rbac.authorization.k8s.io/v1beta1\" -c)"
+    sh "/var/lib/jenkins/go/bin/sonobuoy version || true"
+    sh "/var/lib/jenkins/go/bin/sonobuoy run --wait"
 }
