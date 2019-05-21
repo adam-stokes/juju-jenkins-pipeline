76a27d384565bc8f7741ee5de7e671753cdc01db
Adam Stokes <battlemidget@users.noreply.github.com>
2019-05-21 14:11:08 -0400

    dont use multicore
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -2,5 +2,5 @@ import groovy.transform.Field
 
 @Field cibin = '/var/lib/jenkins/venvs/ci/bin'
 @Field cipy = "${cibin}/python3.6"
-@Field pytest = "${cibin}/pytest -v -s --tb native -ra -n auto"
+@Field pytest = "${cibin}/pytest -v -s --tb native -ra"
 @Field cipaths = "${cibin}:/snap/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin"
