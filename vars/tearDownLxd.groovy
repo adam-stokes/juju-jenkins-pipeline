8c2c5a8023e095dac7de4bac4bf67ed936b5eab1
Adam Stokes <battlemidget@users.noreply.github.com>
2019-05-30 10:17:04 -0400

    shouldnt need sudo for lxc
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/tearDownLxd.groovy
+++ vars/tearDownLxd.groovy
@@ -2,8 +2,8 @@
  *
  */
 def call() {
-    def lxc_yaml = readYaml text:sh(script: "sudo lxc list --format yaml", returnStdout: true)
+    def lxc_yaml = readYaml text:sh(script: "lxc list --format yaml", returnStdout: true)
     lxc_yaml.each { image ->
-        sh "sudo lxc delete --force ${image.name} || true"
+        sh "lxc delete --force ${image.name} || true"
     }
 }
