c38ee44e173eb0590cb4e2eedba4dbbcf6d98286
Adam Stokes <battlemidget@users.noreply.github.com>
2019-06-03 18:05:49 -0400

    on run jenkins tag tasks
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/installToolsJenkaas.groovy
+++ vars/installToolsJenkaas.groovy
@@ -41,6 +41,6 @@ def call() {
         sh "export SCAPESTACKCLOUD=${SCAPESTACKCLOUD}"
         sh "export NEADER=${NEADER}"
         sh "export S3LP3=${S3LP3}"
-        sh "cd jobs && /var/lib/jenkins/venvs/ansible/bin/ansible-playbook infra/playbook-jenkins.yml -e 'ansible_python_interpreter=/usr/bin/python3.5' --limit localhost"
+        sh "cd jobs && /var/lib/jenkins/venvs/ansible/bin/ansible-playbook infra/playbook-jenkins.yml -e 'ansible_python_interpreter=/usr/bin/python3.5' --limit localhost --tags 'jenkins'"
     }
 }
