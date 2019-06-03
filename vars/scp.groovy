0d945ddb9d718e90c5498e775298b39cda9837c6
Adam Stokes <battlemidget@users.noreply.github.com>
2019-06-03 13:38:46 -0400

    recursive scp
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/scp.groovy
+++ vars/scp.groovy
@@ -1,5 +1,5 @@
 // cp's a file to remote
 
 def call(hostname, src, dst) {
-    sh "scp -i /var/lib/jenkins/.ssh/cdkbot_rsa -oStrictHostKeyChecking=no ${src} ${hostname}:${dst}"
+    sh "scp -i /var/lib/jenkins/.ssh/cdkbot_rsa -oStrictHostKeyChecking=no -r ${src} ${hostname}:${dst}"
 }
