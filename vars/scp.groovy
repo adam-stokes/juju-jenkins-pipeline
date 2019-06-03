ebb25710b4aa8162b7a962c65534bf4326380353
Adam Stokes <battlemidget@users.noreply.github.com>
2019-06-03 11:45:13 -0400

    update scp cmd
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/scp.groovy
+++ vars/scp.groovy
@@ -1,5 +1,5 @@
 // cp's a file to remote
 
-def call(ipaddr, src, dst) {
-    sh "scp -i /var/lib/jenkins/.ssh/cdkbot_rsa -oStrictHostKeyChecking=no ${src} ubuntu@${ipaddr}:${dst}"
+def call(hostname, src, dst) {
+    sh "scp -i /var/lib/jenkins/.ssh/cdkbot_rsa -oStrictHostKeyChecking=no ${src} ${hostname}:${dst}"
 }
