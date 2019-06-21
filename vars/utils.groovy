766c82a374c7611954b6287e6c6fdb41fd031609
Adam Stokes <battlemidget@users.noreply.github.com>
2019-06-21 09:56:12 -0400

    update venv paths
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -1,6 +1,5 @@
 import groovy.transform.Field
 
-@Field cibin = '/var/lib/jenkins/venvs/ci/bin'
-@Field cipy = "${cibin}/python3.6"
-@Field pytest = "${cibin}/pytest -v -s --tb native -ra"
-@Field cipaths = "${cibin}:/snap/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin"
+@Field cipy = "workon jenkins && python3.6"
+@Field pytest = "workon jenkins && pytest -v -s --tb native -ra"
+@Field cipaths = "/snap/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin"
